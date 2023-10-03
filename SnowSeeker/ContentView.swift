58114b8641919ec88b80702025837d8492bb7adf
mlwelker <nutria.watts.0g@icloud.com>
2023-10-03 17:53:28 -0500

    sheet(item:) bound to optional (no isShowing boolean)

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -1,20 +1,21 @@
 
 import SwiftUI
 
+struct User: Identifiable {
+    var id = "Taylor Swift"
+}
+
 struct ContentView: View {
+    @State private var selectedUser: User? = nil
+    
     var body: some View {
-        NavigationView {
-            NavigationLink {
-                Text("Hello world")
-            } label: {
-                Text("New Secondary")
+        Text("Hello world")
+            .onTapGesture {
+                selectedUser = User()
             }
-            .navigationTitle("Primary")
-            
-            Text("Secondary")
-            
-            Text("Tertiary")
-        }
+            .sheet(item: $selectedUser) { user in
+                Text(user.id)
+            }
     }
 }
 
