da2dd71000b80a66c86b893f543bdd9982c669d0
mlwelker <nutria.watts.0g@icloud.com>
2023-10-03 23:15:55 -0500

    imported JSON data + basic list navigation view + Resort struct

--- SnowSeeker/ContentView.swift
+++ SnowSeeker/ContentView.swift
@@ -2,25 +2,34 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var searchText = ""
-    let allNames = ["Subh", "Vina", "Melvin", "Stefanie"]
+    let resorts: [Resort] = Bundle.main.decode("resorts.json")
     
     var body: some View {
         NavigationView {
-            List(filteredNames, id: \.self) { name in
-                Text(name)
+            List(resorts) { resort in
+                NavigationLink {
+                    Text(resort.name)
+                } label: {
+                    Image(resort.country)
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 40, height: 25)
+                        .clipShape(RoundedRectangle(cornerRadius: 5))
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 5)
+                                .stroke(.black, lineWidth: 0.25)
+                        )
+                    
+                    VStack(alignment: .leading) {
+                        Text(resort.name)
+                            .font(.headline)
+                        
+                        Text("\(resort.runs) runs")
+                            .foregroundStyle(.secondary)
+                    }
+                }
             }
-            .searchable(text: $searchText, prompt: "Look for something")
-            .navigationTitle("Searching")
-        }
-    }
-    
-    var filteredNames: [String] {
-        if searchText.isEmpty {
-            return allNames
-        } else {
-//            return allNames.filter { $0.contains(searchText)}
-            return allNames.filter { $0.localizedCaseInsensitiveContains(searchText)}
+            .navigationTitle("Resorts")
         }
     }
 }
