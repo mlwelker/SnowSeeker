2752d1f68b0106f2a03bbfba5803abffc7da7caa
mlwelker <nutria.watts.0g@icloud.com>
2023-10-04 18:49:59 -0500

    load / save functionality

--- SnowSeeker/Favorites.swift
+++ SnowSeeker/Favorites.swift
@@ -6,7 +6,12 @@ class Favorites: ObservableObject {
     private let saveKey = "Favorites"
     
     init() {
-        // load saved data
+        if let data = UserDefaults.standard.data(forKey: saveKey) {
+            if let decoded = try? JSONDecoder().decode(Set<String>.self, from: data) {
+                resorts = decoded
+                return
+            }
+        }
         resorts = []
     }
     
@@ -27,6 +32,8 @@ class Favorites: ObservableObject {
     }
     
     func save() {
-        // write out data
+        if let data = try? JSONEncoder().encode(resorts) {
+            UserDefaults.standard.set(data, forKey: saveKey)
+        }
     }
 }
